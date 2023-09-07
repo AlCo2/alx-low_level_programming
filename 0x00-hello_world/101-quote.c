918dec12db8082555e167b8d1673b258b3742b9b
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-07 08:11:13 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
-
-/*
- *main - Entry point 
+/**
+ * main - Entry point 
  * Description: 'the program's description'  
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
