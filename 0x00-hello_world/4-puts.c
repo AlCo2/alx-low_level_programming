86c94abfe8f974c07f976964be3a75362d7508bd
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-07 15:29:01 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+
+/**
  * main-Entry point
- * Description:'the program's description'    
- */
+ * Description:'the program's description'
+*/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
