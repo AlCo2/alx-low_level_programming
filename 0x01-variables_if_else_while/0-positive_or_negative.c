d862702105f5acc8ac4d284cb58ff9d093d2b3fb
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-08 17:14:21 +0100

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,10 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-n > 0 ? printf("is positive") : printf("is negative");
+if (n > 0)
+    printf("is positive");
+else
+    printf("is negative");
+
 return (0);
 }
