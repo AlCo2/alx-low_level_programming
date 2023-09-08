be577ca60c5788a2408af5cd7df6be44880029cf
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-08 17:15:06 +0100

    f

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-	char ch = 'a';
-	char CH = 'A';
-	while (ch <= 'z')
-	{
-		putchar(ch);
-		++ch;
-	}
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		++CH;
-	}
-	putchar('\n');
-	return (0);
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+	putchar(ch);
+	++ch;
+}
+while (CH <= 'Z')
+{
+	putchar(CH);
+	++CH;
+}
+putchar('\n');
+return (0);
 }
