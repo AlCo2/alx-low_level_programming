b36a2bf441133bc655a82257d12e73e94191da1f
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-21 14:52:54 +0100

    finished

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,24 @@
 #include "main.h"
+
 /**
- * print_number -print number putchar
- * @n:integer
- * Return: void
+ * print_number - function that prints an integer
+ * @n: Number to be printed
+ * Return: returns 10 to power enumponent
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n *= -1;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+	num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * infinite_add - adds two numbers.
- * @n1:char
- * @n2:char.
- * @r:char
- * @size_r:int
- * Return:char.
+ * @n1: first number
+ * @n2:second number
+ * @r:store result
+ * @size_r:size of stored result
+ *
+ * Return:0.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m, n;
+	int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
 		;
@@ -18,9 +20,11 @@ int i, j, k, l, m, n;
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
+
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
+
 		if (i >= 0)
 			n += n1[i] - '0';
 		if (j >= 0)
@@ -33,6 +37,7 @@ int i, j, k, l, m, n;
 		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
+
 	if (i >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
