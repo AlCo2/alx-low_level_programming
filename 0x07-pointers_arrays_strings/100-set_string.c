3fa11121e3516f591fc5feb40e28db6d8247e737
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-25 15:15:09 +0100

    finish

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * set_string - sets the value of a pointer to a char
  * @s: pointer to change
@@ -8,5 +8,5 @@
  */
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
