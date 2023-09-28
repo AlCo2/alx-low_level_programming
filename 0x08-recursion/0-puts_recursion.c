83b5db009e193ea658008b065f46250394b85e03
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-28 14:47:22 +0100

    first task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include <main.h>
 
-
 void _puts_recursion(char *s)
 {
-
-  if(*s=='\0'){
-    _putchar('\n');
-    return;
-  }
+  if(*s == '\0')
+{
+  _putchar('\n');
+  return;
+}
   _putchar(*s);
   *s++;
   _puts_recursion(s);
