4ac56cd9efff2b3a358b9b5c8feab68915d73161
Alco2 <abdou.lahboub.wwe@gmail.com>
2023-09-28 17:23:44 +0100

    finish first task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,15 @@
 #include <main.h>
+/**
+ * _puts_recursion - function to print
+ *
+ * @s: this is string
+ *
+ * Return: Always 0 (Success)
+ */
+
 void _puts_recursion(char *s)
 {
-if(*s == '\0')
+if (*s == '\0')
 {
 _putchar('\n');
 return;
