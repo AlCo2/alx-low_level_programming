aeefed2552425aea096f02c3fd05953af363f9eb
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-03 20:25:44 +0100

    betty style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,8 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *node;
 
-	if(*head == NULL)
-		return -1;
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		temp = (*head)->next;
