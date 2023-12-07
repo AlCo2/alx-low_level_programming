7c818d490ca9f736c969ced198a18111f30bc469
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-07 23:11:39 +0100

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_dlistint - function to print doubly linked_list
+ *
+ * @h: doubly linked_list
+ *
+ * Return: size of doublyLinkedList
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * dlistint_len - return length of d linked list
+ *
+ * @h: doubly linked list
+ *
+ * Return: size
+*/
 size_t dlistint_len(const dlistint_t *h);
 {
 	int count = 0;
 
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
 	while (h != NULL)
 	{
 		h = h->next;
