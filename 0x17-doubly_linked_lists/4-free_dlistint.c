cbe5d3a9b538de2dbdee51eb280173a842df480b
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-08 02:28:12 +0100

    task 3 and 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - funtion to free doubly linked list
+ *
+ * @head: doubly linked list
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	while (head->prev != NULL)
+	{
+	       head = head->prev;	
+	}
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = head->next;
+	}
+}
