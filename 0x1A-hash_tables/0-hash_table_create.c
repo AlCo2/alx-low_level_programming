a077e0bdbd26a7f6d77bdd17982f82bb73a258e0
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-21 20:58:16 +0100

    make all values null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable = malloc(sizeof(hash_table_t));
+	int i = 0;
 
 	if (hashTable == NULL)
 		return (NULL);
@@ -17,6 +18,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hashTable->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 	if (hashTable->array == NULL)
 		return (NULL);
+	while (i < size)
+	{
+		hashTable->array[i] = NULL;
+		i++;
+	}
 	return (hashTable);
 }
 
