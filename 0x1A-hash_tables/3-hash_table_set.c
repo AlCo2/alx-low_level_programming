4b150f2a90819991ca919b2b20db32746438dfaf
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-22 00:48:37 +0100

    task to insert item to hashtable

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,69 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function to set value for key in hashtable
+ *
+ * @ht: the HashTable
+ * @key: the key
+ * @value: value to map with key
+ *
+ * Return: 1 in success, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_item = create_item(key, value);
+	const unsigned char *unsigned_key = (const unsigned char *)key;
+	unsigned long int index = key_index(unsigned_key, ht->size);
+	hash_node_t *temp = ht->array[index];
+	hash_node_t *prev;
+
+	if (temp == NULL)
+	{
+		ht->array[index] = new_item;
+		return (1);
+	}
+	else
+	{
+		while (temp != NULL)
+		{
+			if (strcmp(key, temp->key) == 0)
+			{
+				free(temp->value);
+				temp->value = malloc(sizeof(value) + 1);
+				if (temp->value == NULL)
+					return (0);
+				strcpy(temp->value, value);
+				return (1);
+			}
+			if (temp->next == NULL)
+				prev = temp;
+			temp = temp->next;
+		}
+		prev->next = new_item;
+		return (1);
+	}
+	return (0);
+}
+/**
+ * create_item - function to create items
+ *
+ * @key: the key
+ * @value: the value
+ *
+ * Return: created item
+*/
+hash_node_t *create_item(const char *key, const char *value)
+{
+	hash_node_t *item = malloc(sizeof(hash_node_t));
+
+	if (item == NULL)
+		return (NULL);
+	item->key = malloc(strlen(key) + 1);
+	if (item->key == NULL)
+		return (NULL);
+	item->value = malloc(strlen(value) + 1);
+	if (item->value == NULL)
+		return (NULL);
+	strcpy(item->key, key);
+	strcpy(item->value, value);
+	return item;
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,7 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function to get value from key;
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+}
