4c6f69591f7ca7c991d71cc3d28a5dd5e020face
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-21 21:15:03 +0100

    hash function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - function to implements hash
+ *
+ * @str: the word before hash
+ *
+ * Return: the size
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned int hash = 5318;
+	int c;
+
+	while (c = *str++)
+		hash = hash * 33 + c;
+	return hash;
+}
+
