4d7e2625c6ac7191231ede7136b75af9841f4f40
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-22 21:09:11 +0100

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,29 @@
 #include "hash_tables.h"
 /**
- * hash_table_get - function to get value from key;
+ * hash_table_get - function to get value for key in hashtable
+ *
+ * @ht: the HashTable
+ * @key: the key
+ *
+ * Return: value
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (*key == '\0' || !key || !ht)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
 }
+
