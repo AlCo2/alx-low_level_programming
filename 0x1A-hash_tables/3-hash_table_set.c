66c6384a4b6e0ceb1b3ceec566d821bac0761208
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-22 21:01:32 +0100

    add handle for strdup

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,9 +32,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_item == NULL)
 		return (0);
 	new_item->key = strdup(key);
+	if (new_item->key == NULL)
+	{
+		free(new_item);
+		return (0);
+	}
 	new_item->value = strdup(value);
+	if (new_item->value == NULL)
+	{
+		free(new_item->key);
+		free(new_item);
+		return (0);
+	}
 	new_item->next = ht->array[index];
 	ht->array[index] = new_item;
+
 	return (1);
 }
 
