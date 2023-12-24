3b52596d4ac625e9dbd5725cf910880212d888f4
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-24 23:35:45 +0100

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print full hash table
+ *
+ * @ht: hashtable
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	int j = 0;
+
+	printf("{");
+	for (i = 0;i < ht->size;i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (j != 0 )
+			{
+				printf(",");
+			}
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			j++;
+
+		}
+	}
+	printf("}\n");
+}
+
