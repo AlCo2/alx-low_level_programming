881c04483455c61111e51ed35dcaaccc5f8e7bb6
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-12-24 23:42:05 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,33 +1,37 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_print - print full hash table
+ * hash_table_print - Prints a hash table.
  *
  * @ht: hashtable
-*/
+ */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *node;
 	unsigned long int i;
-	hash_node_t *temp;
-	int j = 0;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
-	printf("{");
-	for (i = 0;i < ht->size;i++)
-	{
-		temp = ht->array[i];
-		while (temp)
-		{
-			if (j != 0 && temp->next != NULL)
-			{
-				printf(",");
-			}
-			printf("'%s': '%s'", temp->key, temp->value);
-			temp = temp->next;
-			j++;
 
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
 		}
 	}
 	printf("}\n");
 }
-
