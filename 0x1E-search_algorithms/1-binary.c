3ad037342ba0ab56eccad505e25be2e456913192
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-11 22:52:26 +0000

    add >=

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t r = size - 1;
 	size_t m, i;
 
-	while (l < r)
+	while (l <= r)
 	{
 		m = (l + r) / 2;
 		printf("Searching in array: ");
